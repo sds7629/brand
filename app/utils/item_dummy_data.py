9807046b9b53faf11d9696eb1338cc86f8fe16e5
sds7629 <sds7629@naver.com>
2024-07-01 22:25:54 +0900

    edit item_dummy_data

--- app/utils/item_dummy_data.py
+++ app/utils/item_dummy_data.py
@@ -3,13 +3,6 @@ import os
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from pymongo.errors import BulkWriteError
-
-DB_NAME = os.environ.get("MONGO_DB", "weeber")
-
-client = AsyncIOMotorClient()
-db = client[DB_NAME]
-item_collection = AsyncIOMotorCollection(db, "item")
-
 import random
 
 import faker_commerce
@@ -19,6 +12,16 @@ from faker import Faker
 from app.entities.category.category_codes import CategoryCode
 from app.utils.enums.color_codes import ColorCode
 from app.utils.enums.size_codes import SizeCode
+
+import certifi
+
+ca = certifi.where()
+DB_NAME = os.environ.get("MONGO_DB")
+
+client = AsyncIOMotorClient(DB_NAME, tlsCAFile=ca)
+db = client["FSO"]
+
+item_collection = AsyncIOMotorCollection(db, "item")
 
 fake = Faker("ko_KR")
 fake.add_provider(faker_commerce.Provider)
@@ -40,7 +43,18 @@ color_list: list[ColorCode] = [
     ColorCode.BLACK,
     ColorCode.WHITE,
 ]
-data_mount = 10000
+data_mount = 100
+image_urls_list = [
+    "https://wimg.mk.co.kr/news/cms/202302/26/news-p.v1.20230224.233387a1bc574656b1f57f2324ada54e_P1.png",
+    "https://i.pinimg.com/736x/4c/e8/b8/4ce8b8eb97d4a948551df946feea3b2a.jpg",
+    "https://i.pinimg.com/564x/47/e9/74/47e9742914b497aab11a9e8983db2ad1.jpg",
+    "https://i.pinimg.com/564x/94/b7/a8/94b7a8f0e2cd584d43b9229d019c3674.jpg",
+    "https://i.pinimg.com/564x/ce/ad/6b/cead6b8c0915d16dd14c6e5979841d95.jpg",
+    "https://i.pinimg.com/564x/40/2e/67/402e6772ad229813db3c894ee2ab2a0c.jpg",
+    "https://i.pinimg.com/564x/a3/15/e3/a315e3cf628792239eef6cb7d06a4587.jpg",
+    "https://i.pinimg.com/564x/40/e9/73/40e973555e53590be908cd7820fa3673.jpg",
+]
+
 details = [
     "높은 밀도의 코튼 소재",
     "웨이스트 밴드 드로우 스트링",
@@ -48,13 +62,13 @@ details = [
     "테스트 디테일1",
     "밴드 쩔어서 공연도함",
     "가나다라마바사",
-    "오늘 날씨 너무 더움','" "곧 있으면 장마시작",
+    "오늘 날씨 너무 더움 ㅇㅅㅇ",
+    "곧 있으면 장마시작",
 ]
-
 
 name = [fake.ecommerce_name() for _ in range(data_mount)]
 price = [fake.random_int(min=8000, max=20000) for _ in range(data_mount)]
-image_urls = [[fake.image_url() for _ in range(3)] for _ in range(data_mount)]
+image_urls = [[random.choice(image_urls_list) for _ in range(3)] for _ in range(data_mount)]
 description = [fake.paragraph() for _ in range(data_mount)]
 registration_date = [fake.date_time_between() for _ in range(data_mount)]
 item_quantity = [100 for _ in range(data_mount)]
@@ -62,7 +76,6 @@ size = [random.choice(size_list) for _ in range(data_mount)]
 color = [random.choice(color_list) for _ in range(data_mount)]
 category_codes = [random.choice(category_list) for _ in range(data_mount)]
 details = [[random.choice(details) for _ in range(3)] for _ in range(data_mount)]
-
 
 df = pd.DataFrame()
 df["name"] = name
@@ -80,6 +93,11 @@ records = df.to_dict(orient="records")
 
 
 async def insert_all() -> None:
+    try:
+        await client.admin.command("ping")
+        print("Connection Done")
+    except Exception as e:
+        print(e)
     try:
         await item_collection.insert_many(records)
         print(f"inserted {len(records)} records")
